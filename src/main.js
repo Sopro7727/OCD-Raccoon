42c7a59e03acc8bec94fce44eb0ca3d35d81774d
Noah Walker <nwalker7727@gmail.com>
2023-05-21 12:28:25 -0700

    Edited File names

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 let config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 480,
-    scene: [Menu, Play]
+    width: 1000,
+    height: 800,
+    scene: [Puzzle]
 }
 
 let game = new Phaser.game(config);
 
 let borderUISize = game.config.height/15;
 let borderPadding = borderUISize/3;
-
+let centerX = game.config.width/2;
+let widthSpacer = game.config.width/5;
+let halfHeight = game.config.height/2;
+let cursors = null;
 let keyLEFT, keyRIGHT;
